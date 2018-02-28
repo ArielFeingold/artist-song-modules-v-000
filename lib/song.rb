c7eb209fb6e6c2611cccda90841e69b4eaf4b12d
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-28 09:35:08 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -12,6 +12,9 @@ class Song
     @@songs << self
   end
 
+  def find_by_name(name)
+    all.detect{|a| a.name == name}
+  end
   # def self.find_by_name(name)
   #   @@songs.detect{|a| a.name == name}
   # end
