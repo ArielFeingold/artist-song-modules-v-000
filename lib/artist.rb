1e3fee7c6a1ad99f3599d3336dd26d51f54b4fa5
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-28 10:13:12 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,21 +11,27 @@ class Artist
 
   @@artists = []
 
-  def self.all
-    @@artists
-  end
+  def self.find_by_name(name)
+      @@artists.detect{|a| a.name == name}
+    end
 
-  def initialize
-    @@artists << self
-    @songs = []
-  end
+    def initialize
+      @@artists << self
+      @songs = []
+    end
 
-  def add_song(songs)
-    songs.each { |song| add_song(song) }
-  end
+    def self.all
+      @@artists
+    end
 
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
+    def add_song(song)
+      @songs << song
+      song.artist = self
+    end
+
+    def add_songs(songs)
+      songs.each { |song| add_song(song) }
+    end
+
 
 end
