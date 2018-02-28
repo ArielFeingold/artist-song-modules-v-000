51a5969dbf73fc08a73827c7811f0561eb411b71
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-28 10:15:13 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -16,7 +16,7 @@ class Artist
     end
 
     def initialize
-      self.class.all << self
+      super
       @songs = []
     end
 
@@ -25,7 +25,7 @@ class Artist
     end
 
     def add_song(song)
-      super
+      @songs << song
       song.artist = self
     end
 
