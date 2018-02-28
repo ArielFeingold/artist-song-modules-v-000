e032e3a9298f08d39fb73f38c5faebbd12657232
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-28 09:35:48 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -13,8 +13,8 @@ class Artist
   #   @@artists.detect{|a| a.name == name}
   # end
 
-  def find_by_name(name)
-    all.detect{|a| a.name == name}
+  def self.find_by_name(name)
+    self.all.detect{|a| a.name == name}
   end
   
   def initialize
