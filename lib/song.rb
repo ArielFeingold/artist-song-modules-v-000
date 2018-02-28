d19a247b452201ba5a12674c8d208bd28a272188
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-28 10:20:36 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Song
   extend Memorable::ClassMethods
-  # include Memorable::InstanceMethods
+  include Memorable::InstanceMethods
   extend Findable::ClassMethods
   include Paramable::InstanceMethods
 
@@ -14,10 +14,6 @@ class Song
   def artist=(artist)
     @artist = artist
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 
   def self.all
     @@songs
