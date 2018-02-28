258ae456524fea22d3787a3892e5f9bafeba3794
Ariel Feingold <feingold.ariel@gmail.com>
2018-02-28 09:11:52 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,11 +1,11 @@
 module Memorable
   module ClassMethods
     def reset_all
-      @@songs.clear
+      self.all.clear
     end
 
     def count
-      @@artists.count
+      self.all.count
     end
   end
 end
